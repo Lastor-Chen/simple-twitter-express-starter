1505e16330cc76485eb2362f773aff689de8760a
TomatoSoup <jonathantang0126@gmail.com>
2019-11-26 01:53:29 +0800

    [User model] 設定user資料結構, 增加註冊頁面與相關路由

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const tweetsController = require('../controllers/tweetsControllers')
 const adminController = require('../controllers/adminController.js')
+const userController = require('../controllers/userController.js')
+
 
 
 module.exports = app => {
@@ -8,5 +10,8 @@ module.exports = app => {
 
   app.get('/admin', (req, res) => res.redirect('/admin/tweets'))
   app.get('/admin/tweets', adminController.getTweets)
+
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', userController.signUp)
 
 }
