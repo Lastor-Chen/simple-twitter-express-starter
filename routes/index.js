dc067df0756822aec6df759fe5f31d976abfddc7
TomatoSoup <jonathantang0126@gmail.com>
2019-11-26 00:56:12 +0800

    [環境建置] 增加管理者後台路由與後台view

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 const tweetsController = require('../controllers/tweetsControllers')
+const adminController = require('../controllers/adminController.js')
+
+
 module.exports = app => {
   app.get('/', (req, res) => res.redirect('/tweets'))
   app.get('/tweets', tweetsController.getTweets)
+
+  app.get('/admin', (req, res) => res.redirect('/admin/tweets'))
+  app.get('/admin/tweets', adminController.getTweets)
 
 }
