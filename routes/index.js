05b6a25dd6f81322fc4341e62fbdc17d6a7fcb5d
TomatoSoup <jonathantang0126@gmail.com>
2019-11-28 08:51:50 +0800

    [user註冊] 新增註冊頁面, 相關路由與controller

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const tweetsController = require('../controllers/tweetsController')
+const userController = require('../controllers/userController.js')
 
 module.exports = app => {
   app.get('/', (req, res) => res.redirect('/tweets'))
   app.get('/tweets', tweetsController.getTweets)
+
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', userController.signUp)
 }
