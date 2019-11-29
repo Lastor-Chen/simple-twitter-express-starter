27edf91877471d5b366d633bd07b405b4848502a
TomatoSoup <jonathantang0126@gmail.com>
2019-11-29 22:21:28 +0800

    [新增功能] 表單驗證及驗證結果提示

--- controllers/userCtrller.js
+++ controllers/userCtrller.js
@@ -8,14 +8,26 @@ module.exports = {
   },
 
   signUp: (req, res) => {
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null),
-      introduction: req.body.introduction
-    }).then(user => {
-      res.redirect('/signin')
-    })
+    if (req.body.passwordCheck !== req.body.password) {
+      req.flash('error_messages', '兩次密碼不一致，請重新檢查')
+      return res.redirect('/signup')
+    } else {
+      User.findOne({ where: { email: req.body.email } }).then(user => {
+        if (user) {
+          req.flash('error_messages', '信箱已被其他使用者註冊')
+          return res.redirect('/signup')
+        } else {
+          User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null),
+            introduction: req.body.introduction
+          }).then(user => {
+            req.flash('success_messages', '已成功註冊帳號')
+            return res.redirect('/signin')
+          })
+        }
+      })
+    }
   }
 }
-
