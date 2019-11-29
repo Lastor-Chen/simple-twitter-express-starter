154fb6032849e756d476c4298a97d40283879811
TomatoSoup <jonathantang0126@gmail.com>
2019-11-30 00:13:12 +0800

    [UI] main.hbs的nav bar追加依照登入狀態顯示使用者名稱與登出

--- app.js
+++ app.js
@@ -19,14 +19,16 @@ app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use(session({ secret: 'LastWendyTomatoBurger', resave: false, saveUninitialized: false }))
+
 app.use(passport.initialize())
 app.use(passport.session())
 
-app.use(session({ secret: 'LastWendyTomatoBurger', resave: false, saveUninitialized: false }))
 app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = helpers.getUser(req)
   next()
 })
 
