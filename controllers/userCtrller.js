8f5cd8e2793f6821b53a1980675b939f672cfd05
TomatoSoup <jonathantang0126@gmail.com>
2019-11-29 23:16:09 +0800

    [新增功能] 新增登入頁面與實作登入登出功能

--- controllers/userCtrller.js
+++ controllers/userCtrller.js
@@ -29,5 +29,20 @@ module.exports = {
         }
       })
     }
+  },
+
+  signInPage: (req, res) => {
+    return res.render('signin')
+  },
+
+  signIn: (req, res) => {
+    req.flash('success_messages', '成功登入！')
+    res.redirect('/tweets')
+  },
+
+  logout: (req, res) => {
+    req.flash('success_messages', '登出成功！')
+    req.logout()
+    res.redirect('/signin')
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const tweetsCtrller = require('../controllers/tweetsCtrller')
 const userCtrller = require('../controllers/userCtrller.js')
 const adminTweetsCtrller = require('../controllers/adminTweetsCtrller')
 
-module.exports = app => {
+module.exports = (app, passport) => {
   app.get('/', (req, res) => res.redirect('/tweets'))
   app.get('/tweets', tweetsCtrller.getTweets)
 
@@ -11,4 +11,8 @@ module.exports = app => {
 
   app.get('/signup', userCtrller.signUpPage)
   app.post('/signup', userCtrller.signUp)
+
+  app.get('/signin', userCtrller.signInPage)
+  app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userCtrller.signIn)
+  app.get('/logout', userCtrller.logout)
 }
