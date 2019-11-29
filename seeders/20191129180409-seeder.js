b0c84e693a98eb95f31a922ccdad24f91654778f
wendy hsiao <wendy19940331@gmail.com>
2019-11-30 02:08:32 +0800

    [加入] 新增seeder.js，載入bcrypt-nodejs、faker

---
+++ seeders/20191129180409-seeder.js
@@ -0,0 +1,13 @@
+'use strict';
+const bcrypt = require('bcrypt-nodejs')
+const faker = require('faker')
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+
+  },
+
+  down: (queryInterface, Sequelize) => {
+
+  }
+};
