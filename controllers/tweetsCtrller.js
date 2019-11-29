c1d3966a17a7ab3cc9427d380297e2fb40a9d4a7
Lastor <lastoraya@gmail.com>
2019-11-29 14:26:58 +0800

    [加入] 陽春 tweets 頁面、基礎後台機制

--- controllers/tweetsCtrller.js
+++ controllers/tweetsCtrller.js
@@ -1,5 +1,18 @@
+const db = require('../models')
+const Tweet = db.Tweet
+const User = db.User
+
 module.exports = {
-  getTweets: (req, res) => {
-    res.render('tweets')
+  getTweets: async (req, res) => {
+    try {
+      const tweets = await Tweet.findAll({ order: [['createdAt', 'DESC']] })
+      const users = await User.findAll()
+
+      res.render('tweets', { tweets, users })
+
+    } catch (err) {
+      console.error(err.toString())
+      res.status(500).json(err.toString())
+    }
   }
 }
