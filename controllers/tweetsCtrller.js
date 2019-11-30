22c4de91c4366e6dab96fac714720504b8be63ad
Lastor <lastoraya@gmail.com>
2019-11-30 10:01:51 +0800

    [加入] getTweets 輸出 Model 資料到頁面

--- controllers/tweetsCtrller.js
+++ controllers/tweetsCtrller.js
@@ -5,8 +5,23 @@ const User = db.User
 module.exports = {
   getTweets: async (req, res) => {
     try {
-      const tweets = await Tweet.findAll({ order: [['createdAt', 'DESC']] })
-      const users = await User.findAll()
+      const [tweets, users] = await Promise.all([
+        Tweet.findAll({
+          order: [['id', 'DESC']],  // 最新順
+          include: [{ all: true }] }),
+        User.findAll({ 
+          order: [['id', 'ASC']],
+          include: [{ all: true }]
+        })
+      ]) 
+
+      // 頁面 Date 資訊
+      tweets.forEach(tweet => {
+        tweet.date = tweet.createdAt.toLocaleDateString()
+        tweet.time = tweet.createdAt.toLocaleTimeString().slice(0, -6)
+        tweet.countReplies = tweet.Replies.length
+        tweet.countLikes = tweet.Likes.length
+      })
 
       res.render('tweets', { tweets, users })
 
