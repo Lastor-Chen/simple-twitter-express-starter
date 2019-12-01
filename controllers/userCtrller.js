bb17ce295990a8b7151d549263b058d489e04543
Lastor <lastoraya@gmail.com>
2019-12-01 14:18:22 +0800

    [修改] userCtrller.signUp，重構並加入未填 check

--- controllers/userCtrller.js
+++ controllers/userCtrller.js
@@ -2,34 +2,32 @@ const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
 
+// custom module
+const { checkSignUp } = require('../lib/checker.js')
+
 module.exports = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
 
-  signUp: (req, res) => {
-    if (req.body.passwordCheck !== req.body.password) {
-      req.flash('error', '兩次密碼不一致，請重新檢查')
-      return res.redirect('/signup')
-    }
-    User.findOne({ where: { email: req.body.email } }).then(user => {
-      if (user) {
-        req.flash('error', '信箱已被其他使用者註冊')
-        return res.redirect('/signup')
-      }
-
+  signUp: async (req, res) => {
+    try {
       const input = { ...req.body }  // 深拷貝，保護原始資料
-      input.password = bcrypt.hashSync(input.password, bcrypt.genSaltSync(10), null)
-      User.create(input)
-        .then(user => {
-          req.flash('success', '已成功註冊帳號')
-          return res.redirect('/signin')
-        })
-        .catch(err => {
-          console.error(err.toString())
-          res.status(500).json({ status: 'serverError', message: err.toString() })
-        })
-    })
+
+      // check input
+      const error = await checkSignUp(input)
+      if (error) return res.render('signup', { error })
+
+      input.password = bcrypt.hashSync(input.password, 10)
+      await User.create(input)
+
+      req.flash('success', '已成功註冊帳號')
+      res.redirect('/signin')
+
+    } catch (err) {
+      console.error(err.toString())
+      res.status(500).json({ status: 'serverError', message: err.toString() })
+    }
   },
 
   signInPage: (req, res) => {
