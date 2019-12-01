fbb5d535810dc9a972e70de7b6bbe40a13b61678
Lastor <lastoraya@gmail.com>
2019-12-01 17:32:03 +0800

    [移除] npm 套件 body-parser，微調 app.js

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const helpers = require('./_helpers');
 const exphbs = require('express-handlebars')
-const db = require('./models')
-const bodyParser = require('body-parser')
 const passport = require('./config/passport')
 const flash = require('connect-flash')
 const session = require('express-session')
 
-
+// ======================================
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({
   defaultLayout: 'main',
@@ -17,7 +15,7 @@ app.engine('hbs', exphbs({
 }))
 app.set('view engine', 'hbs')
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(session({
   secret: 'LastWendyTomatoBurger',
