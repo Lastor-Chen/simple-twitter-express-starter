5ba208771b99b41b5c89350daf3ceb2c1c8e1106
wendy hsiao <wendy19940331@gmail.com>
2019-12-03 22:27:41 +0800

    [重構] 修改User model 與 Like、Tweet 的關係

--- models/user.js
+++ models/user.js
@@ -12,6 +12,11 @@ module.exports = (sequelize, DataTypes) => {
     User.hasMany(models.Tweet)
     User.hasMany(models.Like)
     User.hasMany(models.Reply)
+    User.belongsToMany(models.Tweet, {
+      through: models.Like,
+      foreignKey: 'UserId',
+      as: 'LikedTweets'
+    })
     User.belongsToMany(User, {
       through: models.Followship,
       foreignKey: 'followingId',
