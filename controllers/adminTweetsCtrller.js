dc3b55f817d10be404d23a5558c21604877eed8a
TomatoSoup <jonathantang0126@gmail.com>
2019-12-05 01:25:48 +0800

    [加入] getTweets, 傳入hbs前整理tweets內容至50字

--- controllers/adminTweetsCtrller.js
+++ controllers/adminTweetsCtrller.js
@@ -1,6 +1,37 @@
+const db = require('../models')
+const Tweet = db.Tweet
+const User = db.User
+
+const helpers = require('../_helpers')
+
 module.exports = {
-  getTweets: (req, res) => {
-    res.render('admin/tweets')
-  }
+  getTweets: async (req, res) => {
+    try {
+      const reqUser = helpers.getUser(req)
+      const [tweets, users] = await Promise.all([
+        Tweet.findAll({
+          order: [['id', 'DESC']],
+          include: [{ all: true }]
+        }),
+        User.findAll({
+          order: [['id', 'ASC']],
+          include: 'Followers'
+        })
+      ])
+
+      tweets.forEach(tweet => {
+        tweet.date = tweet.createdAt.toLocaleDateString()
+        tweet.time = tweet.createdAt.toLocaleTimeString().slice(0, -6)
+        tweet.description = tweet.description.slice(0, 50)
+      })
+
+      res.render('admin/tweets', { tweets })
+
+    } catch (err) {
+      console.error(err)
+      res.status(500).json(err.toString())
+    }
+  },
+
 }
 
