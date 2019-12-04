13af66e4e9660efe16270a7e12af8ff771931d2d
wendy hsiao <wendy19940331@gmail.com>
2019-12-05 02:25:41 +0800

    [加入] getTweet 傳入資料至 tweet reply 頁面

--- controllers/tweetsCtrller.js
+++ controllers/tweetsCtrller.js
@@ -1,6 +1,5 @@
 const db = require('../models')
-const Tweet = db.Tweet
-const User = db.User
+const { Tweet, User, Reply } = db
 
 module.exports = {
   getTweets: async (req, res) => {
@@ -8,12 +7,13 @@ module.exports = {
       const [tweets, users] = await Promise.all([
         Tweet.findAll({
           order: [['id', 'DESC']],  // 最新順
-          include: [{ all: true }] }),
-        User.findAll({ 
+          include: [{ all: true }]
+        }),
+        User.findAll({
           order: [['id', 'ASC']],
           include: 'Followers'
         })
-      ]) 
+      ])
 
       // 頁面 Date 資訊
       tweets.forEach(tweet => {
@@ -36,6 +36,38 @@ module.exports = {
     }
   },
   getTweet: async (req, res) => {
-    res.render('tweet')
+    try {
+      const showedTweet = await Tweet.findByPk(req.params.tweet_id, {
+        include: [User, 'LikedUsers',
+          {
+            model: Reply, include: [User],
+            order: [[Reply, 'id', 'DESC']]
+          }],
+      })
+
+      const showedUser = await User.findByPk(showedTweet.UserId, {
+        include: [Tweet, 'Followings', 'Followers', 'LikedTweets']
+      })
+
+      // 頁面 Tweets 資訊
+      showedTweet.date = showedTweet.createdAt.toLocaleDateString()
+      showedTweet.time = showedTweet.createdAt.toLocaleTimeString().slice(0, -3)
+      showedTweet.countReplies = showedTweet.Replies.length
+      showedTweet.countLikes = showedTweet.LikedUsers.length
+      showedTweet.isLiked = showedTweet.LikedUsers.some(likedUser => req.user.id === likedUser.id)
+
+      // 頁面 Replies 資訊
+      const replies = showedTweet.Replies
+      replies.forEach(reply => {
+        reply.date = reply.createdAt.toLocaleDateString()
+        reply.time = reply.createdAt.toLocaleTimeString().slice(0, -3)
+      })
+
+      res.render('tweet', { showedTweet, showedUser, replies })
+
+    } catch (err) {
+      console.error(err)
+      res.status(500).json({ status: 'serverError', message: err.toString() })
+    }
   }
 }
